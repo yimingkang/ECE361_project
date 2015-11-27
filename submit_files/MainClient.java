478960a6c96b2f21b5f14e4187d4f28771df2c8c
Ryan Kang <yimingkang@hotmail.com>
2015-11-27 11:29:50 -0500

    Add file to submit

---
+++ submit_files/MainClient.java
@@ -0,0 +1,38 @@
+/*
+Group code: y0z3
+Zexuan Wang 998851773
+Yiming Kang 998676730
+*/
+import java.net.*;
+import java.util.*;
+import java.io.*;
+
+public class MainClient {
+    public static Socket socket;
+    public static double estimatedTimeout;
+
+    public static void main(String[] args){
+        try{
+            // find delay
+            RoutingClient router = new RoutingClient();
+            socket = router.getSock();
+            estimatedTimeout = router.getEstimatedRTT();
+            System.out.println("Esimated RTT is: " + estimatedTimeout);
+
+            // get file name from client
+            System.out.println("Enter file name:");
+            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+            String fileName = reader.readLine();
+            System.out.println("File name is: " + fileName);
+
+
+            // send it off to a file client
+            CCClient sender = new CCClient(socket, estimatedTimeout, fileName);
+            sender.sendFile();
+            while(!socket.isClosed()){
+                Thread.sleep(1000);
+            }
+        } catch (Exception e) {e.getStackTrace();}
+        System.out.println("Exiting...");
+    }
+}
---
+++ submit_files/README.txt
@@ -0,0 +1,12 @@
+Group code: y0z3
+Zexuan Wang 998851773
+Yiming Kang 998676730
+*******************************************************
+-------------------	ECE361 CLIENT ---------------------
+*******************************************************
+
+- to compile the client simply type:
+>>make
+- to run the client simply type:
+>>java MainClient
+and the program will prompt you for the filename you want to transfer
